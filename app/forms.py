04169ebe419e39b32081fdaa69873ff54ede8a68
Dvwhite81 <Dvwhite1981@gmail.com>
2023-09-09 23:02:07 -0400

    Menu - Complete

--- app/forms.py
+++ app/forms.py
@@ -6,6 +6,6 @@ from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    employee_number = StringField('Employee number', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    employee_number = StringField("Employee number", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Login")
